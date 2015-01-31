c4abe9191b72fe5cb19b35211588fe11e48fe500
Paul Fitzgerald <paulfitz99@gmail.com>
2015-01-31 14:35:46 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,10 +3,10 @@ chords = ["A", "B", "C", "D", "E", "F", "G", "Am", "Bm", "Cm", "Dm", "Em", "Fm",
 puts "Choose a level between 1-5 (one being most difficult, five being the easiest)"
 answer = gets.to_i
 
-while chords.length < chords.length + 1
-	chords.each do |x| 
-	y = Random.rand(1..chords.length - 1)
-	puts chords[y]
-	sleep answer/1.5
-end
+	while chords.length < chords.length + 1
+		chords.each do |x| 
+		y = Random.rand(1..chords.length - 1)
+		puts chords[y]
+		sleep answer/1.5
+	end
 end
